f72214e6b8d1c0ae9c586fefa438e43c4ec9c2fc
Вовус <nikitin.vld@gmail.com>
2023-06-28 19:23:30 +0600

    ТЗТЗ

---
+++ StringString.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+int main()
+{
+	std::string myString = "Hello Skillbox";
+
+	std::cout << "Строковая переменная: " << myString << std::endl;
+
+	std::cout << "Длина строки:" << myString.length() << std::endl;
+
+	std::cout << "Первый символ:" << myString[0] << std::endl;
+
+	std::cout << "Последний символ:" << myString[myString.length() - 1] << std::endl;
+
+
+	return 0;
+}
